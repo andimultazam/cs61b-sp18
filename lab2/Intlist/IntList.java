44bfe00fe300cdecfe115f4ff2eebf283e25f087
andimultazam <andi.multazam@gmail.com>
2019-11-30 21:33:38 +0800

    fix null case

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null && B == null) {
+            return null;
+        }
         IntList temp = A;
         while(temp.rest != null) {
             temp = temp.rest;
@@ -94,6 +97,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null && B == null) {
+            return null;
+        }
         IntList res = new IntList(A.first, null);
         IntList ptr = res;
         A = A.rest;
