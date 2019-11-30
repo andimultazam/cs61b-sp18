88401dd4e02afab8666a5d02c60f9a028784699d
andimultazam <andi.multazam@gmail.com>
2019-11-30 21:36:11 +0800

    fix issue

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,7 +81,7 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        if (A == null && B == null) {
+        if (A == null) {
             return null;
         }
         IntList temp = A;
@@ -97,7 +97,7 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        if (A == null && B == null) {
+        if (A == null) {
             return null;
         }
         IntList res = new IntList(A.first, null);
