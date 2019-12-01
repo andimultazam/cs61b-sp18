2df60e28aea372570c4e279c303497836e06a303
andimultazam <andi.multazam@gmail.com>
2019-12-01 14:23:20 +0800

    add methods and tested

---
+++ proj0/Planet.java
@@ -0,0 +1,104 @@
+/**
+ * Planet
+ * Author: Andi Wahyu Multazam
+ */
+public class Planet {
+
+    /* Define variables */
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    /* Constructor */
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        this.xxPos = xP;
+        this.yyPos = yP;
+        this.xxVel = xV;
+        this.yyVel = yV;
+        this.mass = m;
+        this.imgFileName = img;
+    }
+
+    /* Constructor to copy object */
+    public Planet(Planet p) {
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    /**
+     * Calculate distance of two planets
+     * @param p Planet to calculate the distance to
+     * @return distance
+     */
+    public double calcDistance(Planet p) {
+        double dx = this.xxPos - p.xxPos;
+        double dy = this.yyPos - p.yyPos;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+
+    /**
+     * Calculate Force exerted by other planet p
+     * @param p Planet to calculate the force with
+     * @return Force of the two planets
+     */
+    public double calcForceExertedBy(Planet p) {
+        double distance = this.calcDistance(p);
+        return (6.67e-11 * this.mass * p.mass) / (distance*distance);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+         double force = this.calcForceExertedBy(p);
+         double dx = p.xxPos - this.xxPos;
+         double r = this.calcDistance(p);
+         return (force * dx) / r;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+         double force = this.calcForceExertedBy(p);
+         double dy = p.yyPos - this.yyPos;
+         double r = this.calcDistance(p);
+         return (force * dy) / r;
+     }
+
+    public double calcNetForceExertedByX(Planet[] planets) {
+        double netForce = 0;
+        for (Planet p : planets) {
+            if (this == p) {
+                netForce = 0;
+            }
+            else {
+                netForce += this.calcForceExertedByX(p);
+            }
+        }
+        return netForce;
+     }
+
+    public double calcNetForceExertedByY(Planet[] planets) {
+        double netForce = 0;
+        for (Planet p : planets) {
+            if (this == p) {
+                netForce = 0;
+            }
+            else {
+                netForce += this.calcForceExertedByY(p);
+            }
+        }
+        return netForce;
+    }
+
+    public void update(double dt, double Fx, double Fy) {
+        double ax = Fx / this.mass;
+        double ay = Fy / this.mass;
+        this.xxVel = this.xxVel + dt * ax;
+        this.yyVel = this.yyVel + dt * ay;
+        this.xxPos = this.xxPos + dt * (this.xxVel);
+        this.yyPos = this.yyPos + dt * (this.yyVel);
+    }
+}
