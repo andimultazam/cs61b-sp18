4e09eba63474036521689e63ae7cfdeb41f28c91
andimultazam <andi.multazam@gmail.com>
2019-12-12 01:00:43 +0800

    fix style

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,9 +1,21 @@
+/**
+ * This class is intended to test Flik.java.
+ * @author andimultazam
+ */
 public class HorribleSteve {
+    /**
+     * maximum number to test.
+     */
+    private static final int MAX_SIZE = 500;
+    /**
+     * main methods to start the test.
+     * @param args input from user
+     */
     public static void main(String [] args) {
         int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        for (int j = 0; i < MAX_SIZE; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
-                break; // break exits the for loop!
+                break;
             }
         }
         System.out.println("i is " + i);
