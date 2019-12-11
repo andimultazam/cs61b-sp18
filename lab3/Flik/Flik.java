621ae693cf43cb8c7f86169075c45bd8229ef095
andimultazam <andi.multazam@gmail.com>
2019-12-12 01:00:30 +0800

    fix style

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,5 +1,13 @@
-/** An Integer tester created by Flik Enterprises. */
+/** An Integer tester created by Flik Enterprises.
+ *  @author Flik
+ * */
 public class Flik {
+    /**
+     *
+     * @param a input a
+     * @param b input b
+     * @return boolean true if equal, false otherwise
+     */
     public static boolean isSameNumber(Integer a, Integer b) {
         return a.equals(b);
     }
